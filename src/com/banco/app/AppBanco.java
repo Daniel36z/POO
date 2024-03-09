2f3ee0f6316438686606f4e3cf353214d42a91a0
Daniel Mejía <danielmejia282@gmail.com>
2024-03-08 19:05:28 -0500

    cambio 2

--- src/com/banco/app/AppBanco.java
+++ src/com/banco/app/AppBanco.java
@@ -6,15 +6,11 @@ public class AppBanco {
     public static void main(String[] args) {
         CuentaBancaria miCuenta  = new CuentaBancaria();
         CuentaBancaria cuenta2  = new CuentaBancaria();
-        miCuenta.Consignar(100);
-        miCuenta.Transferir(97,cuenta2);
+        miCuenta.Consignar(300);
+        miCuenta.Transferir(80,cuenta2);
         miCuenta.MostrarSaldo();
         cuenta2.MostrarSaldo();
-
-
     }
 }
 
 
-
-// cometario un comentario  nnbbbnvmmm
